29874a17f8e2f3aadeedf6e3d220b4f240196c5b
Udayakumar Rayala <uday.rayala@gmail.com>
2012-08-06 20:59:26 +0100

    Uday Removing unneccessary reporting in file

--- src/main/scala/ruk/tlb/ScalaTestTLBReporter.scala
+++ src/main/scala/ruk/tlb/ScalaTestTLBReporter.scala
@@ -5,7 +5,6 @@ import org.scalatest.events.{SuiteCompleted, TestFailed, SuiteAborted, Event}
 import tlb.factory.TlbFactory
 import tlb.utils.SystemEnvironment
 import collection.mutable
-import java.io.{FileOutputStream, File}
 
 class ScalaTestTLBReporter extends Reporter {
   val server = TlbFactory.getTalkToService(new SystemEnvironment())
@@ -27,33 +26,19 @@ class ScalaTestTLBReporter extends Reporter {
     failedSuites.append(suiteClassName)
   }
 
-  private def reportToServer(suiteClassName:String, duration:Long, failed :Boolean) {
-    reportInFile("Suite class name: %s, duration: %s, failed: %s".format(suiteClassName, duration, failed))
-    server.testClassFailure(suiteClassName, failed)
-    server.testClassTime(suiteClassName, duration)
-  }
-
-  private def blah(suiteClassName:Option[String], duration:Option[Long])(func: (String, Long)=> Unit) {
+  private def record(suiteClassName:Option[String], duration:Option[Long])(func: (String, Long)=> Unit) {
     (suiteClassName, duration) match {
       case (Some(s), Some(d)) if s != "org.scalatest.ScalaTestTLBSuite" => func(s, d)
       case _ =>
     }
   }
 
-  private def reportInFile(str:String) {
-    val file = new File("/tmp/scalatest-reporter.log")
-    val fileOutputStream: FileOutputStream = new FileOutputStream(file, true)
-    fileOutputStream.write(str.getBytes)
-    fileOutputStream.write("\n".getBytes)
-    fileOutputStream.close()
-  }
-
   def apply(event: Event) {
     try {
       event match {
-        case e: SuiteAborted => blah(e.suiteClassName, e.duration)(suiteAborted)
-        case e: TestFailed => blah(e.suiteClassName, e.duration)(testFailed)
-        case e: SuiteCompleted => blah(e.suiteClassName, e.duration)(suiteCompleted)
+        case e: SuiteAborted => record(e.suiteClassName, e.duration)(suiteAborted)
+        case e: TestFailed => record(e.suiteClassName, e.duration)(testFailed)
+        case e: SuiteCompleted => record(e.suiteClassName, e.duration)(suiteCompleted)
         case _ =>
       }
     }
