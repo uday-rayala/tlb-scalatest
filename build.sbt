43db56a45ab99702185878a2023f256b634b8488
Udayakumar Rayala <uday.rayala@gmail.com>
2012-08-06 21:29:27 +0100

    Uday Moved libraries from lib folder to sbt dependencies

--- build.sbt
+++ build.sbt
@@ -2,7 +2,10 @@ name := "tlb-scalatest"
 
 version:= "1.0"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" withSources()
+libraryDependencies ++= Seq(
+    "org.scalatest" %% "scalatest" % "1.6.1" withSources(),
+    "log4j" % "log4j" % "1.2.14"
+)
 
 parallelExecution in Test := false
 
--- src/main/scala/ruk/tlb/ScalaTestTLBReporter.scala
+++ src/main/scala/ruk/tlb/ScalaTestTLBReporter.scala
@@ -10,20 +10,13 @@ class ScalaTestTLBReporter extends Reporter {
   val server = TlbFactory.getTalkToService(new SystemEnvironment())
   val failedSuites:mutable.ListBuffer[String] = mutable.ListBuffer()
 
-  private def suiteAborted(suiteClassName:String, duration:Long) {
-    reportToServer(suiteClassName, duration, true)
-  }
-
-  private def suiteCompleted(suiteClassName:String, duration:Long) {
-    if (failedSuites.contains(suiteClassName)){
-      reportToServer(suiteClassName, duration, true)
-    } else {
-      reportToServer(suiteClassName, duration, false)
+  def apply(event: Event) {
+    event match {
+      case e: SuiteAborted => record(e.suiteClassName, e.duration)(suiteAborted)
+      case e: TestFailed => record(e.suiteClassName, e.duration)(testFailed)
+      case e: SuiteCompleted => record(e.suiteClassName, e.duration)(suiteCompleted)
+      case _ =>
     }
-  }
-
-  private def testFailed(suiteClassName:String, duration:Long) {
-    failedSuites.append(suiteClassName)
   }
 
   private def record(suiteClassName:Option[String], duration:Option[Long])(func: (String, Long)=> Unit) {
@@ -33,17 +26,24 @@ class ScalaTestTLBReporter extends Reporter {
     }
   }
 
-  def apply(event: Event) {
-    try {
-      event match {
-        case e: SuiteAborted => record(e.suiteClassName, e.duration)(suiteAborted)
-        case e: TestFailed => record(e.suiteClassName, e.duration)(testFailed)
-        case e: SuiteCompleted => record(e.suiteClassName, e.duration)(suiteCompleted)
-        case _ =>
-      }
+  private def suiteAborted(suiteClassName:String, duration:Long) {
+    reportToServer(suiteClassName, duration, failed = true)
+  }
+
+  private def suiteCompleted(suiteClassName:String, duration:Long) {
+    if (failedSuites.contains(suiteClassName)){
+      reportToServer(suiteClassName, duration, failed = true)
+    } else {
+      reportToServer(suiteClassName, duration, failed = false)
     }
-    catch {
-      case e: Throwable => reportInFile(e.toString + "\n" + e.getStackTraceString)
-    }
+  }
+
+  private def reportToServer(suiteClassName:String, duration:Long, failed :Boolean) {
+    server.testClassFailure(suiteClassName, failed)
+    server.testClassTime(suiteClassName, duration)
+  }
+
+  private def testFailed(suiteClassName:String, duration:Long) {
+    failedSuites.append(suiteClassName)
   }
 }
